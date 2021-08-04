b9b6c179039f1bc7892e468da1628c8cf93470d6
mucheol <ehdanfeorkfl@gmail.com>
2021-08-05 02:48:52 +0900

    common.js 오류 수정

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -1,9 +1,9 @@
 (function (window, document) {
     $("#headerLogo").hover(function(){
-    	$(this).css('width','13vw');
+    	$(this).css('width','10vw');
     	$(this).css('top','-0.6vw');
     },function(){
-    	$(this).css('width','11vw');
+    	$(this).css('width','9vw');
     	$(this).css('top','0.4vw');
     });
 })(window, document);
