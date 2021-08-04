81c6c9d23f0ec97d81a151fcbc61b0d8c12d0972
mucheol <ehdanfeorkfl@gmail.com>
2021-08-05 03:22:28 +0900

    img 관련 js 높이 변경

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -1,7 +1,7 @@
 (function (window, document) {
     $("#headerLogo").hover(function(){
     	$(this).css('width','10vw');
-    	$(this).css('top','-0.6vw');
+    	$(this).css('top','-0.1vw');
     },function(){
     	$(this).css('width','9vw');
     	$(this).css('top','0.4vw');
